281bde8f05a99d7f9d2cae99ad2e5ac070835726
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-03 18:55:39 +0000

    FFImageLoading plugin initialized in IOS project

--- Timeline/Timeline.iOS/AppDelegate.cs
+++ Timeline/Timeline.iOS/AppDelegate.cs
@@ -22,6 +22,8 @@ namespace Timeline.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+			FFImageLoading.Forms.Platform.CachedImageRenderer.Init();     
+			
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
